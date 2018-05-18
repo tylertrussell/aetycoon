ecd9ee5c62ba206750405176a9bcaaaff2b60ad0
Tyler Trussell <tigertrussell@gmail.com>
2018-05-18 03:30:01 -0700

    import test

--- catnado/properties/test/test_import.py
+++ catnado/properties/test/test_import.py
@@ -1,15 +1,24 @@
+import logging
 import pkgutil
 
+from catnado import properties as catnado_properties
 from catnado.testing.testcase import SimpleAppEngineTestCase
 
 
 class CatnadoPropertyImportTest(SimpleAppEngineTestCase):
 
   def test_importing_catnado_properties(self):
-    prefix = '{}.'.format(self.__module__.__name__)
-    for _, name, _ in pkgutil.iter_modules(self.__path__, prefix=prefix):
+    prefix = '{}.'.format(catnado_properties.__name__)
+    module_names = [
+      module_name
+      for _, module_name, _
+      in pkgutil.iter_modules(catnado_properties.__path__, prefix=prefix)
+      if 'test' not in module_name
+    ]
+    for name in module_names:
       try:
+        logging.debug('importing {}'.format(name))
         __import__(name)
-      except e:
-        print 'Error importing module {}'.format(name)
+      except:
+        logging.error('Error importing module {}'.format(name))
         raise
