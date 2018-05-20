df0d31764e61781ec95b2ac65d722d933bfe98b9
Tyler Trussell <tigertrussell@gmail.com>
2018-05-19 18:15:42 -0700

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name='gae-catnado',
-  version='0.0.1.dev6',
+  version='0.0.1.dev7',
   description='Google App Engine datastore properties and helpers',
   long_description=long_description,
   long_description_content_type='text/markdown',
